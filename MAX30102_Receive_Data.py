8785c160b1046c468043bc0a8bb2ba722719858b
Ember Ipek <74475571+EmberIpek@users.noreply.github.com>
2026-06-30 12:58:03 -0400

    Enhance MAX30102 data reception with plotting
    Added data collection and plotting for red and IR samples.

--- MAX30102_Receive_Data.py
+++ MAX30102_Receive_Data.py
@@ -1,14 +1,21 @@
 import socket
 import struct
-import time
-import matplotlib
-import scipy
+from datetime import datetime
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+
+count = 0
+red_samples = []
+ir_samples = []
+time_received = []
+
 
 print("Listening on port", UDP_PORT)
 
@@ -28,30 +35,24 @@ while True:
               ", Red LED: ", red,
               ", IR LED: ", ir,
               ", checksum: ", checksum)
-    except struct.error:
-        print("Packet unpacking failed")import socket
-import struct
-import time
-import matplotlib
-import scipy
-
-UDP_IP = "0.0.0.0"
-UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-
-print("Listening on port", UDP_PORT)
-
-while True:
-    packet, addr = sock.recvfrom(1024)
-    print("Received", len(packet), "bytes from", addr)
-    
-    # > = big endian, f = float
-    try:
-        data = struct.unpack("<f", packet)
-        temperature = data[0]
         
-        print("UDP received! Unpacked data: ", temperature)
+        if(count < 1000):
+            count += 1
+            red_samples.append(red)
+            ir_samples.append(ir)
+            time_received.append(datetime.now())
+        else:
+            break
+        
     except struct.error:
         print("Packet unpacking failed")
+        
+
+plt.plot(time_received, red_samples, label='Red Samples')
+plt.plot(time_received, ir_samples,  label='IR Samples')
+plt.legend()
+plt.title("LED Samples")
+plt.xlabel("time")
+plt.ylabel("ADC value")
+
+plt.show()
